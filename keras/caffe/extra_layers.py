ca4095495ccb9f82590f456406c272cb8cae50af
Natalia Bilenko <nbilenko@gmail.com>
2016-09-01 13:33:23 -0700

    Make LRN2D TF-compatible

--- keras/caffe/extra_layers.py
+++ keras/caffe/extra_layers.py
@@ -1,56 +1,40 @@
 from keras.layers.core import Layer
 
-import theano.tensor as T
-
-
 class LRN2D(Layer):
     """
-    This code is adapted from pylearn2.
-    License at: https://github.com/lisa-lab/pylearn2/blob/master/LICENSE.txt
+    Fixed using https://github.com/fchollet/keras/issues/1549
+    -Natalia Bilenko
     """
 
     def __init__(self, alpha=1e-4, k=2, beta=0.75, n=5, **kwargs):
+        if n % 2 == 0:
+            raise NotImplementedError("LRN2D only works with odd n. n provided: " + str(n))
         super(LRN2D, self).__init__(**kwargs)
         self.alpha = alpha
         self.k = k
         self.beta = beta
         self.n = n
-        if n % 2 == 0:
-            raise NotImplementedError("Only works with odd n")
-      
-    def call(self, x, mask=None):
-        X = x
-        input_dim = X.shape
-        half_n = self.n // 2
-        input_sqr = T.sqr(X)
-        b, ch, r, c = input_dim
-        extra_channels = T.alloc(0., b, ch + 2*half_n, r, c)
-        input_sqr = T.set_subtensor(extra_channels[:, half_n:half_n+ch, :, :],input_sqr)
-        scale = self.k
-        norm_alpha = self.alpha / self.n
-        for i in range(self.n):
-            scale += norm_alpha * input_sqr[:, i:i+ch, :, :]
-        scale = scale ** self.beta
-        return X / scale
-        
+
     def get_output(self, train):
         X = self.get_input(train)
-        input_dim = X.shape
+        b, ch, r, c = K.shape(X)
         half_n = self.n // 2
-        input_sqr = T.sqr(X)
-        b, ch, r, c = input_dim
-        extra_channels = T.alloc(0., b, ch + 2*half_n, r, c)
-        input_sqr = T.set_subtensor(extra_channels[:, half_n:half_n+ch, :, :],input_sqr)
+        input_sqr = K.square(X)
+        extra_channels = K.zeros((b, ch + 2 * half_n, r, c))
+        input_sqr = K.concatenate([extra_channels[:, :half_n, :, :], 
+                                input_sqr,
+                                extra_channels[:, half_n+ch:, :, :]],
+                                axis = 1)
         scale = self.k
-        norm_alpha = self.alpha / self.n
         for i in range(self.n):
-            scale += norm_alpha * input_sqr[:, i:i+ch, :, :]
+            scale += self.alpha * input_sqr[:, i:i+ch, :, :]
         scale = scale ** self.beta
         return X / scale
 
     def get_config(self):
-        return {
-            "alpha":self.alpha,
-            "k":self.k,
-            "beta":self.beta,
-            "n": self.n}
+        config = {"alpha": self.alpha,
+                "k": self.k,
+                "beta": self.beta,
+                "n": self.n}
+        base_config = super(LRN2D, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
